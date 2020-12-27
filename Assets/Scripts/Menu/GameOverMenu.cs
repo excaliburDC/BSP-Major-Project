ff9952e22de5219d5b7d99c93262bd84e9182b9f
meghapandichelvam <57277665+meghapandichelvam@users.noreply.github.com>
2020-12-27 23:29:43 +0530

    Added the GameOver and GameComplete Menu,minor changes in CutScene

---
+++ Assets/Scripts/Menu/GameOverMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameOverMenu : MonoBehaviour
+{
+   
+    public void PlayAgain()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void Ending1()
+    {
+        //SceneManager.LoadScene("Cutscene1");//ending 1
+    }
+    public void Ending2()
+    {
+        //SceneManager.LoadScene("Cutscene2");//ending 2
+    }
+}
